26a3adcf493ad1c2ef45a57e4b7e99cb9c8a5acc
Diego San Vicente <neidan.arq.s@gmail.com>
2025-10-20 15:18:46 -0600

    Add docker-compose file and typeORM for database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AuthorsModule } from './authors/authors.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, AuthorsModule, CategoriesModule],
 })
 export class AppModule {}
+
